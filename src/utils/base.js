0461f55b40d924fde1f6c01e9dd74081393fa7de
huocb <changbin.huo@gientech.com>
2023-04-18 14:45:04 +0800

    初始化项目

---
+++ src/utils/base.js
@@ -0,0 +1,16 @@
+const base = {
+    get() {
+        return {
+            url : "http://localhost:8080/vote/",
+            name: "vote",
+            // 退出到首页链接
+            indexUrl: ''
+        };
+    },
+    getProjectName(){
+        return {
+            projectName: "在线投票管理系统"
+        } 
+    }
+}
+export default base
